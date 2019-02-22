51208fd4526b7316ac1b53d929ef24e0c0ec4140
Kellen <thekeldog@gmail.com>
2019-02-22 12:54:51 -0800

    Kellen (#8)
    * minor tweaks to homepage, setting up wireframe
    
    * Added footer and more wireframing of home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,17 +10,10 @@ import { QueuepageComponent } from './queuepage/queuepage.component';
 import { ModelpageComponent } from './modelpage/modelpage.component';
 import { NavigationbarComponent } from './navigationbar/navigationbar.component';
 import { ActivecasesComponent } from './activecases/activecases.component';
-import { FooterComponent} from './footer/footer.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { AnalystsmallComponent } from './analystpage/analystsmall/analystsmall.component';
-import { AnalystsidebarComponent } from './analystpage/analystsidebar/analystsidebar.component';
-import { AnalystsubpagelistComponent } from './analystpage/analystsubpagelist/analystsubpagelist.component';
-import { AnalystsubpagedetailedComponent } from './analystpage/analystsubpagedetailed/analystsubpagedetailed.component';
-import { AnalysttableComponent } from './analystpage/analysttable/analysttable.component';
-import { AnalystsfastestComponent } from './analystpage/analystsfastest/analystsfastest.component';
-import { AnalystsslowestComponent } from './analystpage/analystsslowest/analystsslowest.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -31,14 +24,7 @@ import { AnalystsslowestComponent } from './analystpage/analystsslowest/analysts
     ModelpageComponent,
     NavigationbarComponent,
     ActivecasesComponent,
-    AnalystsmallComponent,
-    AnalystsidebarComponent,
-    AnalystsubpagelistComponent,
-    AnalystsubpagedetailedComponent,
     FooterComponent,
-    AnalysttableComponent,
-    AnalystsfastestComponent,
-    AnalystsslowestComponent
   ],
   imports: [
     BrowserModule,
