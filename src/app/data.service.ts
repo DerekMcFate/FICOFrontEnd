2a2b1d358fa27bba5fd22c2817950c291340eadb
DerekMcFate <dmcfate@csumb.edu>
2019-03-13 00:26:28 -0700

    XLSX added to project(needs work)

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -3,6 +3,7 @@ import { Analyst } from './analyst.component';
 import { Queue } from './queue.component';
 import { FPRScore } from './fprScore.component';
 import { Case } from './case.component';
+declare var XLSX: any;
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,13 @@ export class DataService {
   fprScore$: FPRScore;
   case$: Case;
 
+  constructor() {
+    if(typeof require !== 'undefined') XLSX = require('xlsx');
+    var workbook = XLSX.readFile('test.xlsx');
+    console.log(workbook)
+  }
 
-  constructor() { }
+  getModels() {
+
+  }
 }
