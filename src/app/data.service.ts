f56a3ea100b4badd98a71ccfb35b45c0ba302cef
DerekMcFate <dmcfate@csumb.edu>
2019-04-03 11:21:22 -0700

    added data to the model cards

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -23,4 +23,20 @@ export class DataService {
   getQueues() {
     return this.http.get('assets/queueData.json');
   }
+
+  groupObjects(array, element) {
+    //This function is designed to organize elements in an array based on one of it's common keys.
+    var i = 0, val, index, values = [], result = [];
+    for (; i < array.length; i++) {
+      val = array[i][element];
+      index = values.indexOf(val);
+      if(index > -1)
+        result[index].push(array[i]);
+      else {
+        values.push(val);
+        result.push([array[i]]);
+      }
+    }
+    return result
+  }
 }
