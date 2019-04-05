42214b2d0c305a2b8a05e64925f4410c309d8e79
DerekMcFate <dmcfate@csumb.edu>
2019-04-05 13:10:48 -0700

    Added data to the .ts for other pages

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -37,6 +37,41 @@ export class DataService {
         result.push([array[i]]);
       }
     }
+    console.log("Grouped Objects!\n");
+    console.log(result);
     return result
+  }
+
+  mergeData(array1, array2, key) {
+    //This function is designed to merge two data arrays based on a key shared between them.
+    var keyMap = {};
+    for(var i = 0; i < 2; i++) {
+      for(var j = 0; j < array1.length; j++) {
+        var id = array1[j][key];
+        if(!keyMap[id]) {
+          keyMap[id] = {};
+        }
+        for(var element in array1[j]) {
+          keyMap[id][element] = array1[j][element];
+        }
+      }
+      for(var j = 0; j < array2.length; j++) {
+        var id = array2[j][key];
+        if(!keyMap[id]) {
+          keyMap[id] = {};
+        }
+        for(var element in array2[j]) {
+          keyMap[id][element] = array2[j][element];
+        }
+      }
+    }
+
+    var newArray = [];
+    for(var property in keyMap) {
+      newArray.push(keyMap[property]);
+    }
+    console.log("Merged Data!\n");
+    console.log(newArray);
+    return newArray;
   }
 }
--- src/app/modelpage/modeloverview/modeloverview.component.ts
+++ src/app/modelpage/modeloverview/modeloverview.component.ts
@@ -9,12 +9,17 @@ import { DataService } from 'src/app/data.service';
 export class ModeloverviewComponent implements OnInit {
 
   fprScores$: Object;
+  queues$: Object;
+  combinedData$: Object;
   
   constructor(private data: DataService) { }
 
   ngOnInit() {
     this.data.getModels().subscribe(
       data => this.fprScores$ = this.data.groupObjects(data, "SCORING_MODEL_NAME")
+    );
+    this.data.getQueues().subscribe(
+      data => this.queues$ = data
     );
   }
 
